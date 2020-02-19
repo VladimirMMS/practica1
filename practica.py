c0926831d9797676291a20e60421bc51b87f49a8
Vladimir <61054610+VladimirMMS@users.noreply.github.com>
2020-02-19 11:08:03 -0800

    Class and Object

--- practica.py
+++ practica.py
@@ -1,7 +1,24 @@
-class Armas:
-    modelo = "47"
-    velocidad = 100
-    habilidad = "Precisa"
+class weapon:
+    standard = "48"
+    velocity = 10
+    skill = "Recarga"
 
-AK = Armas()
-print (AK.habilidad)
+
+print (weapon.standard,weapon.velocity,weapon.skill)
+
+class matter:
+    pass
+
+spaninh = matter()
+math = matter()
+
+spaninh.score = 100
+spaninh.teacher = 'Juan'
+spaninh.school = 'Institute Dubert'
+
+math.score = 75
+math.teacher = 'Maria'
+math.school = 'Institute Alfredo'
+
+print (math.score,math.teacher,math.school)
+print (spaninh.score,spaninh.teacher,spaninh.school)
